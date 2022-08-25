9a41b4889c1374ea9d150606be74431a2a8e35d0
na2na-p <na2na@na2na.dev>
2022-08-26 00:35:34 +0900

    Change expressやめた

--- src/@types/node/process.d.ts
+++ src/@types/node/process.d.ts
@@ -3,9 +3,13 @@ declare module 'process' {
     const process: NodeJS.Process;
     namespace NodeJS {
       interface ProcessEnv extends Dict<string> {
-        SECRET: string
-        DISCORD_WEBHOOK_URL: string
-        PORT: string
+        SECRET?: string
+        DISCORD_WEBHOOK_URL?: string
+        PORT?: string
+				/**
+				 * ## FOR DEBUG USE ONLY!!!!
+				 */
+				SMEE_PROXY_URL?: string
       }
     }
   }
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,37 @@
-import express from 'express';
-import logger from 'morgan';
-import 'reflect-metadata';
 import {config as dotenv} from 'dotenv';
-
-import {Server} from './server.js';
-
-// ルーティングを配列の形で列挙
-import {Index} from '@routes/index.js';
 dotenv();
 
-// ミドルウェアを配列の形で列挙
-const middleware: Function[] | string[] = [
-	logger('dev'),
-	express.json(),
-	express.urlencoded({extended: false}),
-];
-const routes: Function[] | string[] = [
-	Index,
-];
+import {createServer} from 'http';
+import EventSource from 'eventsource';
+import {Webhooks, createNodeMiddleware} from '@octokit/webhooks';
 
-new Server(middleware, routes);
+export class Server {
+	private readonly port: number = (typeof process.env.PORT === 'number' ? parseInt(process.env.PORT) : 3000);
+	private webhooks: Webhooks<unknown> = new Webhooks({secret: process.env.SECRET!});
+
+	constructor() {
+		this.webhooks.onAny(({id, name, payload}) => {
+			console.log(id);
+			console.log(name, 'event received');
+			console.log(payload);
+		});
+
+		if (process.env.NODE_ENV !== 'production') {
+			const app = new EventSource(process.env.SMEE_PROXY_URL!);
+			app.onmessage = (event: any) => {
+				const webhookEvent = JSON.parse(event.data);
+				this.webhooks
+					.verifyAndReceive({
+						id: webhookEvent['x-request-id'],
+						name: webhookEvent['x-github-event'],
+						signature: webhookEvent['x-hub-signature'],
+						payload: webhookEvent.body,
+					})
+					.catch(console.error);
+			};
+		}
+		createServer(createNodeMiddleware(this.webhooks)).listen(this.port);
+	};
+};
+
+new Server();
