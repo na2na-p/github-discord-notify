286bfea0147373ed60e57abb2131ab017df85e62
na2na-p <na2na@na2na.dev>
2022-08-27 21:38:10 +0900

    WIP

--- src/modules/embedCreate.ts
+++ src/modules/embedCreate.ts
@@ -1,33 +1,33 @@
 import {EmbedBuilder as eb} from 'discord.js';
-// import {match} from 'ts-pattern';
+import {match} from 'ts-pattern';
 
-import {EmitterWebhookEvent, EmitterWebhookEventName} from '@octokit/webhooks';
+import {EmitterWebhookEvent as _E, EmitterWebhookEventName} from '@octokit/webhooks';
 
 export class EmbedBuilder<T extends EmitterWebhookEventName> extends eb {
 	// TODO: payloadの型定義見直し
 	constructor(_id: string, name: T, payload: any) {
 		super();
 
-		switch (name) {
-		case 'push': {
-			// TODO: Eliminate any
-			const pl = payload as EmitterWebhookEvent<'push'> & {sender: any, commits: any[]};
-			this.setAuthor({
-				name: pl.sender.login,
-				iconURL: pl.sender.avatar_url ?
-					pl.sender.avatar_url :
-					'https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png',
-			});
-			this.setTitle(`🆕 Pushed by ${pl.sender.login} with ${pl.commits.length} commits`);
-			this.setDescription(payload.head_commit.message);
-			this.setURL(payload.compare_url);
-			pl.commits.forEach((commit) => {
-				// 先頭7文字
-				this.addFields(commit.id.slice(0, 7), commit.message);
-			});
-			console.log('honihogehage');
-		}
-		}
+		console.log(name);
+		this.setEmbed(name, payload);
+
+
+		// TODO: Eliminate any
+		// const pl = payload as EmitterWebhookEvent<'push'> & {sender: any, commits: any[]};
+		// this.setAuthor({
+		// 	name: pl.sender.login,
+		// 	iconURL: pl.sender.avatar_url ?
+		// 		pl.sender.avatar_url :
+		// 		'https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png',
+		// });
+		// this.setTitle(`🆕 Pushed by ${pl.sender.login} with ${pl.commits.length} commits`);
+		// this.setDescription(payload.head_commit.message);
+		// this.setURL(payload.compare_url);
+		// pl.commits.forEach((commit) => {
+		// 	// 先頭7文字
+		// 	this.addFields(commit.id.slice(0, 7), commit.message);
+		// });
+		// console.log('honihogehage');
 
 		console.log(name);
 		if (name === 'check_run') {
@@ -37,5 +37,16 @@ export class EmbedBuilder<T extends EmitterWebhookEventName> extends eb {
 
 		// this.setTitle(payload);
 		this.setColor(0xC239B3);
+	}
+
+	private setEmbed(name: EmitterWebhookEventName, _payload: any) {
+		match(name)
+			.with('push', () => {
+				console.log('push');
+			})
+			.with('check_run', () => {
+				console.log('check_run');
+			})
+			.exhaustive();
 	}
 }
