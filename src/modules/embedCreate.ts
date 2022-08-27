de60ddf991626268b7288b366639b52ecf57c845
na2na-p <na2na@na2na.dev>
2022-08-27 16:59:08 +0900

    WIP

--- src/modules/embedCreate.ts
+++ src/modules/embedCreate.ts
@@ -1,19 +1,24 @@
 import {EmbedBuilder as eb} from 'discord.js';
+// import {match} from 'ts-pattern';
 
-import {EmitterWebhookEventName, EmitterWebhookEvent} from '@octokit/webhooks';
+import {EmitterWebhookEventName} from '@octokit/webhooks';
 
 export class EmbedBuilder<T extends EmitterWebhookEventName> extends eb {
 	// TODO: payloadの型定義見直し
 	constructor(id: string, name: T, payload: any) {
 		super();
+		this.setAuthor({
+			name: 'GitHub',
+			iconURL: 'https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png',
+		});
 		console.log(id);
 		console.log(name, 'event received');
 		if (name === 'check_run') {
 			console.log(name);
 		}
-		console.log(payload.id);
+		console.log(payload);
 
-		this.setTitle('Some Title');
+		// this.setTitle(payload);
 		this.setColor(0xC239B3);
 	}
 }
