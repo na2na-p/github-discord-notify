5cf04b47e0b26d69a490b9f745c81883469c7140
na2na-p <na2na@na2na.dev>
2022-08-27 16:51:37 +0900

    負けました

--- src/modules/embedCreate.ts
+++ src/modules/embedCreate.ts
@@ -1,14 +1,24 @@
 import {EmbedBuilder as eb} from 'discord.js';
 
-import {EmitterWebhookEventName} from '@octokit/webhooks';
+// import {EmitterWebhookEventName} from '@octokit/webhooks';
+import type {WebhookEventName} from '@octokit/webhooks-types';
 
-export class EmbedBuilder<E extends EmitterWebhookEventName> extends eb {
-	// TODO: payloadの型定義見直し
+// やりたい
+// EventPayloadMapに含まれるプロパティのうち、キー名がEの値になっている型を取得したい
+
+export class EmbedBuilder<E extends WebhookEventName> extends eb {
+	// TODO: Eliminate any
 	constructor(id: string, name: E, payload: any) {
 		super();
 		console.log(id);
 		console.log(name, 'event received');
-		console.log(payload.sender);
+		console.log(payload);
+
+		if (name === 'push') {
+			this.setTitle('Push event');
+			console.log(name);
+			console.log(payload);
+		}
 
 		this.setAuthor({
 			name: payload.sender.login,
