2589186ab92503698e4491fb4ddda89e8b754eb6
na2na-p <na2na@na2na.dev>
2022-09-06 15:45:04 +0900

    WIP

--- src/modules/embedCreate.ts
+++ src/modules/embedCreate.ts
@@ -24,26 +24,34 @@ export class EmbedBuilder<T extends keyof WebhookEventMap> extends eb {
 	private setEmbed() {
 		this.setColor(0xC239B3);
 		(() => {
+			switch (this.name) {
+			case 'push':
+				console.log('match with push');
+				// console.log('push');
+				// console.log(this.name);
+				const payload = this.payload as WebhookEventMap['push'];
+				this.setAuthor({
+					name: payload.sender.login,
+					iconURL: payload.sender.avatar_url ?
+						payload.sender.avatar_url :
+						'https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png',
+				});
+				this.setTitle(`[${payload.repository.full_name}]`);
+				this.setDescription(`🆕 Pushed by ${payload.sender.login} with ${payload.commits.length} commits`);
+				this.setURL(payload.compare);
+				payload.commits.forEach((commit: any) => {
+					// 先頭7文字:
+					this.addFields(commit.id.slice(0, 7), commit.message);
+				});
+				break;
+			default:
+				this.setDescription('some event happened');
+				break;
+			}
 			console.log(`still not match with ${this.name}`);
 			match(this.name)
 				.with('push', () => {
-					console.log('match with push');
-					// console.log('push');
-					// console.log(this.name);
-					const payload = this.payload as WebhookEventMap['push'];
-					this.setAuthor({
-						name: payload.sender.login,
-						iconURL: payload.sender.avatar_url ?
-							payload.sender.avatar_url :
-							'https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png',
-					});
-					this.setTitle(`[${payload.repository.full_name}]`);
-					this.setDescription(`🆕 Pushed by ${payload.sender.login} with ${payload.commits.length} commits`);
-					this.setURL(payload.compare);
-					payload.commits.forEach((commit: any) => {
-					// 先頭7文字:
-						this.addFields(commit.id.slice(0, 7), commit.message);
-					});
+
 				})
 				.with('check_run', () => {
 					console.log('check_run');
